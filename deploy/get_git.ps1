360486668e83a2cd3f1093fd14b48f2fd14e70e3
Eric Frankenberger <efrankie@gmail.com>
2024-03-04 22:50:01 -0500

    get_git working

--- deploy/get_git.ps1
+++ deploy/get_git.ps1
@@ -1,27 +1,17 @@
 . ./variables.ps1
 
-# Set the URL of the Git repository
-$repoUrl = "https://github.com/efranken/CS-Surf-ML"
-
-# Set the target folder where you want to download the repository
-$targetFolder = $gitDirectory
-
-# Create a temporary folder for downloading and extracting
-$tempFolder = Join-Path $env:TEMP "GitRepoDownload"
-
-# Ensure the temporary folder exists, and if not, create it
-if (-not (Test-Path $tempFolder)) {
-    New-Item -ItemType Directory -Path $tempFolder | Out-Null
+# Check if the directory exists
+if (Test-Path $gitPath -PathType Container) {
+    # If it exists, delete it
+    Remove-Item -Path $gitPath -Recurse -Force
+    Write-Host "Directory '$gitPath' existed and has been deleted."
+} else {
+    # If it doesn't exist, write a message to the host
+    Write-Host "Directory '$gitPath' does not exist yet."
 }
 
 # Download the ZIP archive
-$zipFile = Join-Path $tempFolder "repo.zip"
-Invoke-WebRequest -Uri $repoUrl -OutFile $zipFile
+git clone $gitUrl $gitPath
 
-# Extract the contents of the ZIP archive
-Expand-Archive -Path $zipFile -DestinationPath $targetFolder -Force
-
-# Clean up: Remove the temporary folder
-Remove-Item -Path $tempFolder -Recurse -Force
 
 Write-Host "Repository contents downloaded to $targetFolder"
--- readme.md
+++ readme.md
@@ -83,3 +83,15 @@ Download the repo and place in a folder that will be used as the working directo
             "pass" "ml" CHANGE THIS LINE TO YOUR PW
         }
     ```
+
+
+## rolling todo
+
+1.  move steamcmd_config to a couple different files
+2.  make a main runner script
+3.  configure database script
+4.  start steam server properly
+5.  connect to it from vm
+6.  get virtual controller working via scripted install
+7.  get the correct versions of openai stuff, or convert if needed
+8.  get it running like i had in the youtube video
